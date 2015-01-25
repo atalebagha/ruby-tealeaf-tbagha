ff0eb7d359064ab2554fe3479a2fff3f127eae6b
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-01-25 19:15:23 +0300

    Basics.rb

---
+++ Basics.rb
@@ -0,0 +1,56 @@
+# RUBY BASICS
+
+# EX 1
+
+puts "John" + "Doe"
+
+# EX 2
+
+a = 2342 / 1000
+
+b = 2342 % 1000 / 100
+
+c = 2342 % 1000 % 100 / 10
+
+d = 2342 % 1000 % 100 % 10
+
+
+# EX 3
+
+movies = {m1: 1999, m2: 1998, m3: 2004, m4: 2005, m5: 2007}
+
+puts movies[:m1]
+puts movies[:m2]
+puts movies[:m3]
+puts movies[:m4]
+puts movies[:m5]
+
+# EX 4
+
+movies = [1999, 1998, 2004, 2005, 2007]
+
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
+
+
+# EX 5
+
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+# EX 6
+
+puts 233.32 * 233.32
+puts 43.3 * 43.3
+puts 5.5 * 5.5
+
+# EX 7
+
+# That a ')' was used in a place where a '}' is expected on line 2
+
+
